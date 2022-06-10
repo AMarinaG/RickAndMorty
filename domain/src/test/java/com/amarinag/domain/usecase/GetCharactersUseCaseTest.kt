a54121dbf50dab6e0d991c1e97b8624c6e1f5a4f
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-10 11:06:44 +0200

    added testing on usecase

---
+++ domain/src/test/java/com/amarinag/domain/usecase/GetCharactersUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.amarinag.domain.usecase
+
+import com.amarinag.domain.AppDispatchers
+import com.amarinag.domain.model.Character
+import com.amarinag.domain.repository.CharacterRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class GetCharactersUseCaseTest {
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    @MockK
+    private lateinit var characterRepository: CharacterRepository
+    private val appDispatchers = AppDispatchers(testDispatcher, testDispatcher, testDispatcher)
+
+    private lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this, relaxed = true)
+        getCharactersUseCase = GetCharactersUseCase(appDispatchers, characterRepository)
+    }
+
+    @Test
+    fun `check that usecase call invoke seccess`() = runTest {
+        val character = Character(
+            1,
+            "Rick",
+            "species",
+            "type",
+            "imageUrl",
+            "locationName",
+            "locationUrl",
+            emptyList()
+        )
+        coEvery { characterRepository.findMatchCharacter(character) }
+
+        getCharactersUseCase()
+
+        coVerify(exactly = 1) { characterRepository.getAll() }
+    }
+}
