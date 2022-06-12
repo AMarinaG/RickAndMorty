19d21a428782625da9f1f5f921661949b0ca42bd
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-12 21:13:30 +0200

    added simple loading content

---
+++ app/src/main/java/com/amarinag/rickandmorty/ui/component/LoadingContent.kt
@@ -0,0 +1,40 @@
+package com.amarinag.rickandmorty.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.amarinag.rickandmorty.R
+
+@Composable
+fun LoadingContent(
+    loading: Boolean,
+    modifier: Modifier = Modifier,
+    loadingContent: @Composable () -> Unit = { DefaultLoading(modifier) },
+    content: @Composable () -> Unit
+) {
+    if (loading) {
+        loadingContent()
+    } else {
+        content()
+    }
+}
+
+
+@Composable
+fun DefaultLoading(modifier: Modifier = Modifier) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+        val progress by animateLottieCompositionAsState(composition)
+        LottieAnimation(
+            composition,
+            { progress },
+            modifier = modifier
+        )
+    }
+}
