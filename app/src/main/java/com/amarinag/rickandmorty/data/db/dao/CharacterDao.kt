3e7a504663a1f7292f7a782b22edb918d15439c3
AMarinaG <alberto.marina.gonzalo@gmail.com>
2022-06-12 20:50:45 +0200

    ignore when add repeated character on db

--- app/src/main/java/com/amarinag/rickandmorty/data/db/dao/CharacterDao.kt
+++ app/src/main/java/com/amarinag/rickandmorty/data/db/dao/CharacterDao.kt
@@ -2,6 +2,7 @@ package com.amarinag.rickandmorty.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.amarinag.rickandmorty.data.db.entity.LocalCharacter
 
@@ -13,6 +14,6 @@ interface CharacterDao {
     @Query("SELECT * FROM character WHERE id = :characterId")
     suspend fun getCharacterById(characterId: Int): LocalCharacter
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertAll(characters: List<LocalCharacter>)
 }
